14483d30220696122b2be4229269068133ba1b9f
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-04-14 20:24:48 -0400

    reg: not necessary anymore

--- reg.h
+++ reg.h
@@ -10,83 +10,81 @@
 
 #include "types.h"
 
-#define reg(addr, name) enum { _ ## name = addr }; _reg _at (_ ## name) name;
-
 // I/O registers
-reg(0x0000, GCSR )
-reg(0x0002, RTC0R)
-reg(0x0003, RTC1R)
-reg(0x0004, RTC2R)
-reg(0x0005, RTC3R)
-reg(0x0006, RTC4R)
-reg(0x0007, RTC5R)
-reg(0x0008, WDTCR)
-reg(0x0009, WDTTR)
-reg(0x000f, GCDR )
-reg(0x0014, MB0CR)
-reg(0x0015, MB1CR)
-reg(0x0016, MB2CR)
-reg(0x0017, MB3CR)
-reg(0x0024, SPCR )
+_reg _at (0x0000) GCSR;
+_reg _at (0x0002) RTC0R;
+_reg _at (0x0003) RTC1R;
+_reg _at (0x0004) RTC2R;
+_reg _at (0x0005) RTC3R;
+_reg _at (0x0006) RTC4R;
+_reg _at (0x0007) RTC5R;
+_reg _at (0x0008) WDTCR;
+_reg _at (0x0009) WDTTR;
+_reg _at (0x000f) GCDR;
+_reg _at (0x0014) MB0CR;
+_reg _at (0x0015) MB1CR;
+_reg _at (0x0016) MB2CR;
+_reg _at (0x0017) MB3CR;
+_reg _at (0x0024) SPCR ;
 
-reg(0x0040, PBDR )
-reg(0x0047, PBDDR)
-reg(0x0050, PCDR )
-reg(0x0055, PCFR )
-reg(0x0070, PEDR )
-reg(0x0074, PECR )
-reg(0x0075, PEFR )
-reg(0x0077, PEDDR)
-reg(0x0078, PEB0R)
-reg(0x0079, PEB1R)
-reg(0x007a, PEB2R)
-reg(0x007b, PEB3R)
-reg(0x007c, PEB4R)
-reg(0x007d, PEB5R)
-reg(0x007e, PEB6R)
-reg(0x007f, PEB7R)
+_reg _at (0x0040) PBDR;
+_reg _at (0x0047) PBDDR;
+_reg _at (0x0050) PCDR;
+_reg _at (0x0055) PCFR;
+_reg _at (0x0070) PEDR;
+_reg _at (0x0074) PECR;
+_reg _at (0x0075) PEFR;
+_reg _at (0x0077) PEDDR;
+_reg _at (0x0078) PEB0R;
+_reg _at (0x0079) PEB1R;
+_reg _at (0x007a) PEB2R;
+_reg _at (0x007b) PEB3R;
+_reg _at (0x007c) PEB4R;
+_reg _at (0x007d) PEB5R;
+_reg _at (0x007e) PEB6R;
+_reg _at (0x007f) PEB7R;
 
-reg(0x00a0, TACSR)
-reg(0x00a1, TAPR )
-reg(0x00a3, TAT1R)
-reg(0x00a4, TACR )
-reg(0x00a5, TAT2R)
-reg(0x00a6, TAT8R)
-reg(0x00a7, TAT3R)
-reg(0x00a8, TAT9R)
-reg(0x00a9, TAT4R)
-reg(0x00aa, TAT10R)
-reg(0x00ab, TAT5R)
-reg(0x00ad, TAT6R)
-reg(0x00af, TAT7R)
+_reg _at (0x00a0) TACSR;
+_reg _at (0x00a1) TAPR;
+_reg _at (0x00a3) TAT1R;
+_reg _at (0x00a4) TACR;
+_reg _at (0x00a5) TAT2R;
+_reg _at (0x00a6) TAT8R;
+_reg _at (0x00a7) TAT3R;
+_reg _at (0x00a8) TAT9R;
+_reg _at (0x00a9) TAT4R;
+_reg _at (0x00aa) TAT10R;
+_reg _at (0x00ab) TAT5R;
+_reg _at (0x00ad) TAT6R;
+_reg _at (0x00af) TAT7R;
 
-reg(0x00c0, SADR )
-reg(0x00c1, SAAR )
-reg(0x00c2, SALR )
-reg(0x00c3, SASR )
-reg(0x00c4, SACR )
-reg(0x00c5, SAER )
+_reg _at (0x00c0) SADR;
+_reg _at (0x00c1) SAAR;
+_reg _at (0x00c2) SALR;
+_reg _at (0x00c3) SASR;
+_reg _at (0x00c4) SACR;
+_reg _at (0x00c5) SAER;
 
-reg(0x00d0, SBDR )
-reg(0x00d1, SBAR )
-reg(0x00d2, SBLR )
-reg(0x00d3, SBSR )
-reg(0x00d4, SBCR )
-reg(0x00d5, SBER )
+_reg _at (0x00d0) SBDR;
+_reg _at (0x00d1) SBAR;
+_reg _at (0x00d2) SBLR;
+_reg _at (0x00d3) SBSR;
+_reg _at (0x00d4) SBCR;
+_reg _at (0x00d5) SBER;
 
-reg(0x00e0, SCDR )
-reg(0x00e1, SCAR )
-reg(0x00e2, SCLR )
-reg(0x00e3, SCSR )
-reg(0x00e4, SCCR )
-reg(0x00e5, SCER )
+_reg _at (0x00e0) SCDR;
+_reg _at (0x00e1) SCAR;
+_reg _at (0x00e2) SCLR;
+_reg _at (0x00e3) SCSR;
+_reg _at (0x00e4) SCCR;
+_reg _at (0x00e5) SCER;
 
-reg(0x00f0, SDDR )
-reg(0x00f1, SDAR )
-reg(0x00f2, SDLR )
-reg(0x00f3, SDSR )
-reg(0x00f4, SDCR )
-reg(0x00f5, SDER )
+_reg _at (0x00f0) SDDR;
+_reg _at (0x00f1) SDAR;
+_reg _at (0x00f2) SDLR;
+_reg _at (0x00f3) SDSR;
+_reg _at (0x00f4) SDCR;
+_reg _at (0x00f5) SDER;
 
 // shadow registers
 extern byte _at (0xdf00) GCSS;
